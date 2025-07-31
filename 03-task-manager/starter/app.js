0323e86001fc0cba26f300a05b715aa4dabde7d9
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-07-30 18:21:18 -0700

    'task-manager-1'

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express');
+const routerTasks = require('./routes/tasks');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/v1/tasks', routerTasks);
+
+app.all('/*', (req, res) => {
+  res.status(404).send('resource not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening port ${PORT}...`);
+});
