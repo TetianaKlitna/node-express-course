78cd321151ba362e9555675641b0497a3373fb94
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-07-31 23:29:55 -0700

    'post'

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,17 +1,27 @@
+const appEnv = require('./config/env');
+const connectDB = require('./db/connect');
+
 const express = require('express');
-const routerTasks = require('./routes/tasks');
 const app = express();
-const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+const routerTasks = require('./routes/tasks');
 app.use('/api/v1/tasks', routerTasks);
 
-app.all('/*', (req, res) => {
+app.all('*', (req, res) => {
   res.status(404).send('resource not found');
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening port ${PORT}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(appEnv.MONGO_URI);
+    app.listen(appEnv.PORT, () => {
+      console.log(`Listening port ${appEnv.PORT}...`);
+    });
+  } catch (error) {
+    console.error(`Error during start: ${error}`);
+  }
+};
+
+start();
--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,13 +1,28 @@
-const getAllTasks = (req, res) => {
-  res.send('all tasks');
+const Task = require('../models/Task');
+
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find();
+  res.json(200).json(tasks);
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const name = req.body.name;
+    if(!name){
+      return res.status(400).json({success: false, error: 'Task name is required.'});
+    }
+    let completed = req.body?.completed || false;
+    const task = await Task.create({ name, completed });
+    res.status(201).json({ success: true, task });
+  } catch (error) {
+    console.error(`Error during create task ${req.body}: ${error.message}`);
+    res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 const getTask = (req, res) => {
-  res.json({id: req.params.id});
+  Task.get;
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
