e91cbbaed30e6b9d34b259e9492202fe78a2a5a5
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-07 20:36:03 -0700

    'error/middleware'

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,98 +1,56 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error');
 
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find();
-    res.status(200).json({ success: true, tasks });
-  } catch (error) {
-    console.error(`Error during fetching  ${error}`);
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find();
+  res
+    .status(200)
+    .json({ success: true, tasks, amount: tasks.length });
+});
 
-const createTask = async (req, res) => {
-  try {
-    const { name, completed } = req.body;
-    const task = await Task.create({ name, completed });
-    res.status(201).json({ success: true, task });
-  } catch (error) {
-    console.error(`Error during creating a task ${req.body}`);
-    console.error(error);
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ success: true, task });
+});
 
-const getTask = async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   if (!taskID || taskID.trim() === '') {
-    return res
-      .status(400)
-      .json({ success: false, error: 'Task ID is required.' });
+    return next(createCustomError(400, 'Task ID is required.'));
   }
-  try {
-    const task = await Task.findById(taskID);
-    if (!task) {
-      return res.status(404).json({
-        success: false,
-        error: `Task with ID: ${taskID} is not found`,
-      });
-    }
-    res.status(200).json({ success: true, task });
-  } catch (error) {
-    console.error(`Error during getting the task with ID: ${taskID}`);
-    console.error(error);
-    res.status(500).json({ success: false, error: error.message });
+  const task = await Task.findById(taskID);
+  if (!task) {
+    return next(createCustomError(404, `Task with ID: ${taskID} is not found`));
   }
-};
+  res.status(200).json({ success: true, task });
+});
 
-const updateTask = async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   if (!taskID || taskID.trim() === '') {
-    return res
-      .status(400)
-      .json({ success: false, error: 'Task id is required.' });
+    return next(createCustomError(400, 'Task ID is required.'));
   }
-  try {
-    const task = await Task.findByIdAndUpdate(taskID, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res
-        .status(404)
-        .json({ success: false, error: 'Task is not found' });
-    }
-    res.status(200).json({ success: true, task });
-  } catch (error) {
-    console.error(
-      `Error during updating the task with id: ${taskID} ${req.body}`
-    );
-    console.error(error);
-    res.status(500).json({ success: false, error: error.message });
+  const task = await Task.findByIdAndUpdate(taskID, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return next(createCustomError(404, `Task with ID: ${taskID} is not found`));
   }
-};
+  res.status(200).json({ success: true, task });
+});
 
-const deleteTask = async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
-  try {
-    if (!taskID || taskID.trim() === '') {
-      return res
-        .status(400)
-        .json({ success: false, error: 'Task ID is required.' });
-    }
-    const task = await Task.findByIdAndDelete(taskID);
-    if (!task) {
-      return res.status(404).json({
-        success: false,
-        error: `Task with ID: ${taskID} is not found.`,
-      });
-    }
-    res.status(200).json({ success: true, task });
-  } catch (error) {
-    console.error(`Error during deleteting the task with ID: ${taskID}`);
-    console.error(error);
-    res.status(500).json({ success: error, error: error.message });
+  if (!taskID || taskID.trim() === '') {
+    return next(createCustomError(400, 'Task ID is required.'));
   }
-};
+  const task = await Task.findByIdAndDelete(taskID);
+  if (!task) {
+    return next(createCustomError(404, `Task with ID: ${taskID} is not found`));
+  }
+  res.status(200).json({ success: true, task });
+});
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
---
+++ 03-task-manager/starter/middleware/error-handler.js
@@ -0,0 +1,15 @@
+const { CustomAPIError } = require('../errors/custom-error');
+
+const errorsHandlerMiddleware = (err, req, res, next) => {
+  console.error(err);
+  if (err instanceof CustomAPIError) {
+    return res
+      .status(err.statusCode)
+      .json({ success: false, msg: err.message });
+  }
+  return res
+    .status(500)
+    .json({ success: false, msg: 'Something went wrong, please try again' });
+};
+
+module.exports = errorsHandlerMiddleware;
