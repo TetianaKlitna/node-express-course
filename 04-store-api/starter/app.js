ad65504d8de7095b1ada13d5077296ddcbfaf27f
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-12 00:06:12 -0700

    'products'

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+
+//routers
+const productsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => { console.log(`Server is listening ${PORT} port...`); });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
