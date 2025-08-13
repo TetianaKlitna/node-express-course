24697afa96ba6082c5b9dacf6fac586f1eb5ec8b
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-12 22:48:04 -0700

    filter/sort query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,34 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async error');
-  res.status(200).json({ msg: 'product testing route' });
+  const products = await Product.find({}).sort('-name price');
+  res.status(200).json({ products, len: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'product route' });
+  const queryObj = {};
+  const { featured, company, name, sort } = req.query;
+  if (featured) {
+    queryObj.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: 'i' };
+  }
+  let result = Product.find(queryObj);
+  if (sort) {
+    const sortList = sort
+      .split(',')
+      .map((item) => item.trim())
+      .join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
+  res.status(200).json({ products, len: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
