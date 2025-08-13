48660c47944bc3b476cd27d12783218251bc4cff
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-12 23:26:30 -0700

    select

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,13 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('-name price');
+  const products = await Product.find({}).select('name price').limit(3);
   res.status(200).json({ products, len: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   const queryObj = {};
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   if (featured) {
     queryObj.featured = featured === 'true' ? true : false;
   }
@@ -26,6 +26,13 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   } else {
     result = result.sort('createdAt');
+  }
+  if (fields) {
+    const fieldsList = fields
+      .split(',')
+      .map((item) => item.trim())
+      .join(' ');
+    result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ products, len: products.length });
